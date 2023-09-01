da214cf0619212af2cd8a07881dabea74937e865
Ethan Hu <ethanhu147@gmail.com>
2023-09-01 12:31:48 -0400

    Added scanners

--- Receipt.java
+++ Receipt.java
@@ -5,7 +5,26 @@ public class Receipt{
     public static void main(String[] args)
     {
         System.out.println("Hello, dear customer! Welcome to my store!");
-        System.out.print("We have apples, bananas, pears, strawberries and watermelons here. High quality guaranteed.");
-        Scanner scan 
+        System.out.println("We have apples, bananas, pears, strawberries and watermelons here. High quality guaranteed.");
+        Scanner scan = new Scanner(System.in);
+        int apple;
+        int banana;
+        int pear;
+        int strawberry;
+        int watermelon;
+        System.out.println("How many apples do you want?");
+        apple = scan.nextInt();
+        System.out.println("How many banana do you want?");
+        banana = scan.nextInt();
+        System.out.println("How many pears do you want?");
+        pear = scan.nextInt();
+        System.out.println("How many strawberries do you want?");
+        strawberry = scan.nextInt();
+        System.out.println("How many watermelons do you want?");
+        watermelon = scan.nextInt();
+        System.out.println("****************************************
+                            *I'm too lazy to actually put words    *
+                            *here. So I will just type some random *");
+
     }
 }
