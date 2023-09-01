4cb75e1b2c247f5cc37904b2be37eabc5963b7be
Ethan Hu <ethanhu147@gmail.com>
2023-09-01 12:12:20 -0400

    Empty

--- Receipt.java
+++ Receipt.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Receipt{
     public static void main(String[] args)
     {
-        System.out.print("Hello, world!");
-        System.out.print("Bye, world!");
+
     }
 }
