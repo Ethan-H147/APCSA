6f2b020b17eae82703da593d2cef33e7f769c300
Ethan Hu <ethanhu147@gmail.com>
2023-09-01 12:17:27 -0400

    welcome message

--- Receipt.java
+++ Receipt.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Receipt{
     public static void main(String[] args)
     {
-
+        System.out.println("Hello, dear customer! Welcome to my store!");
+        System.out.print("We have apples, bananas, pears, strawberries and watermelons here. High quality guaranteed.");
+        Scanner scan 
     }
 }
