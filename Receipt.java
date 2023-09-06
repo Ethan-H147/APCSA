b65f3474ffb5643ed45dcbe4bff1ec994cac00e1
Ethan Hu <ethanhu147@gmail.com>
2023-09-06 13:02:07 -0400

    align

--- Receipt.java
+++ Receipt.java
@@ -30,20 +30,35 @@ public class Receipt{
         System.out.println(" wrote, I'm just making this look like  ");
         System.out.println(" an actual receipt.                     ");
         System.out.println("****************************************");
-        if(apple != 0){
+        if(apple != 0&&apple<10){
         System.out.println(" Apple         "+apple+"      1.49      "+String.format("%.2f", 1.49*apple));
         }
-        if(banana != 0){
+        else if(apple>10){
+        System.out.println(" Apple         "+apple+"     1.49      "+String.format("%.2f", 1.49*apple));
+        }
+        if(banana != 0&&banana<10){
         System.out.println(" Banana        "+banana+"      1.19      "+String.format("%.2f", 1.19*banana));
         }
-        if(pear != 0){
+        else if(banana>10){
+        System.out.println(" Banana        "+banana+"     1.19      "+String.format("%.2f", 1.19*banana));
+        }
+        if(pear != 0&&pear<10){
         System.out.println(" Pear          "+pear+"      2.29      "+String.format("%.2f", 2.29*pear));
         }
-        if(strawberry != 0){
+        else if(pear>10){
+        System.out.println(" Pear          "+pear+"     2.29      "+String.format("%.2f", 2.29*pear));
+        }
+        if(strawberry != 0&&strawberry<10){
         System.out.println(" Strawberry    "+strawberry+"      3.59      "+String.format("%.2f", 3.59*strawberry));
         }
-        if(watermelon != 0){
+        else if(strawberry>10){
+        System.out.println(" Strawberry    "+strawberry+"     3.59      "+String.format("%.2f", 3.59*strawberry));
+        }
+        if(watermelon != 0&&watermelon<10){
         System.out.println(" Watermelon    "+watermelon+"      4.99      "+String.format("%.2f", 4.99*watermelon));
+        }
+        else if(watermelon>10){
+        System.out.println(" Watermelon    "+watermelon+"     4.99      "+String.format("%.2f", 4.99*watermelon));
         }
         double total = 1.49*apple+1.19*banana+2.29*pear+3.59*strawberry+4.99*watermelon;
         System.out.println("              Tax     7.0%      "+String.format("%.2f", total*0.07));
