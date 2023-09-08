4419fd2b8e5bc52eafbdb3d167bec3f98ba561e0
Ethan Hu <ethanhu147@gmail.com>
2023-09-08 14:55:10 -0400

    Perfectly align, finally :(

--- Receipt.java
+++ Receipt.java
@@ -41,30 +41,56 @@ public class Receipt{
         System.out.println(" Apple         "+apple+"     1.49     "+String.format("%.2f", 1.49*apple));
         }
         }   
-        if(banana != 0&&banana<10){
+        
+        if(banana != 0){
+        if(banana<10&&1.19*banana<10){
         System.out.println(" Banana        "+banana+"      1.19      "+String.format("%.2f", 1.19*banana));
         }
-        else if(banana>10){
-        System.out.println(" Banana        "+banana+"     1.19      "+String.format("%.2f", 1.19*banana));
+        else if(banana<10&&1.19*banana>=10){
+        System.out.println(" Banana        "+banana+"      1.19     "+String.format("%.2f", 1.19*banana));
         }
-        if(pear != 0&&pear<10){
+        else if(banana>=10){
+        System.out.println(" Banana        "+banana+"     1.19     "+String.format("%.2f", 1.19*banana));
+        }
+        }
+
+        if(pear!= 0){
+        if(pear<10&2.29*pear<10){
         System.out.println(" Pear          "+pear+"      2.29      "+String.format("%.2f", 2.29*pear));
         }
-        else if(pear>10){
-        System.out.println(" Pear          "+pear+"     2.29      "+String.format("%.2f", 2.29*pear));
+        else if(pear<10&&2.29*pear>=10){
+        System.out.println(" Pear          "+pear+"      2.29     "+String.format("%.2f", 2.29*pear));
         }
-        if(strawberry != 0&&strawberry<10){
+        else if(pear>=10){
+        System.out.println(" Pear          "+pear+"     2.29     "+String.format("%.2f", 2.29*pear));
+        }
+        }
+
+
+        if(strawberry!= 0){
+        if(strawberry<10&3.59*strawberry<10){
         System.out.println(" Strawberry    "+strawberry+"      3.59      "+String.format("%.2f", 3.59*strawberry));
         }
-        else if(strawberry>10){
-        System.out.println(" Strawberry    "+strawberry+"     3.59      "+String.format("%.2f", 3.59*strawberry));
+        else if(strawberry<10&&3.59*pear>=10){
+        System.out.println(" Strawberry    "+strawberry+"      3.59     "+String.format("%.2f", 3.59*strawberry));
         }
-        if(watermelon != 0&&watermelon<10){
+        else if(strawberry>=10){
+        System.out.println(" Strawberry    "+strawberry+"     3.59     "+String.format("%.2f", 3.59*strawberry));
+        }
+        }
+
+        if(watermelon!= 0){
+        if(watermelon<10&4.99*watermelon<10){
         System.out.println(" Watermelon    "+watermelon+"      4.99      "+String.format("%.2f", 4.99*watermelon));
         }
-        else if(watermelon>10){
-        System.out.println(" Watermelon    "+watermelon+"     4.99      "+String.format("%.2f", 4.99*watermelon));
+        else if(watermelon<10&&4.99*pear>=10){
+        System.out.println(" Watermelon    "+watermelon+"      4.99     "+String.format("%.2f", 4.99*watermelon));
         }
+        else if(watermelon>=10){
+        System.out.println(" Watermelon    "+watermelon+"     4.99     "+String.format("%.2f", 4.99*watermelon));
+        }
+        }
+
         double total = 1.49*apple+1.19*banana+2.29*pear+3.59*strawberry+4.99*watermelon;
         System.out.println("              Tax     7.0%      "+String.format("%.2f", total*0.07));
         total = total*1.07;
